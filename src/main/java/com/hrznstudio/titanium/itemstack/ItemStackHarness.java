589046981bedf2de126044ff0050f001186590c6
AlphaMode <alphamodeyt@gmail.com>
2022-12-24 01:00:12 -0600

    Allow ItemStackHardness to use Components

--- src/main/java/com/hrznstudio/titanium/itemstack/ItemStackHarness.java
+++ src/main/java/com/hrznstudio/titanium/itemstack/ItemStackHarness.java
@@ -14,7 +14,9 @@ import com.hrznstudio.titanium.api.client.IScreenAddonProvider;
 import com.hrznstudio.titanium.container.addon.IContainerAddon;
 import com.hrznstudio.titanium.container.addon.IContainerAddonProvider;
 import com.hrznstudio.titanium.network.IButtonHandler;
+import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import net.fabricmc.fabric.api.lookup.v1.item.ItemApiLookup;
+import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -25,14 +27,16 @@ import java.util.List;
 public class ItemStackHarness implements IContainerAddonProvider, IScreenAddonProvider, IButtonHandler {
     private final ItemStack itemStack;
     private final IButtonHandler buttonHandler;
-    private final ItemApiLookup<?, Void>[] capabilities;
+    private final List<ItemApiLookup<?, ContainerItemContext>> capabilities;
+    private final List<ComponentKey<?>> components;
     private final IScreenAddonProvider defaultProvider;
 
-    public ItemStackHarness(ItemStack itemStack, IScreenAddonProvider defaultProvider, IButtonHandler buttonHandler, ItemApiLookup<?, Void>... capabilities) {
+    public ItemStackHarness(ItemStack itemStack, IScreenAddonProvider defaultProvider, IButtonHandler buttonHandler, List<ItemApiLookup<?, ContainerItemContext>> capabilities, List<ComponentKey<?>> components) {
         this.itemStack = itemStack;
         this.defaultProvider = defaultProvider;
         this.buttonHandler = buttonHandler;
         this.capabilities = capabilities;
+        this.components = components;
     }
 
     @Override
@@ -40,10 +44,17 @@ public class ItemStackHarness implements IContainerAddonProvider, IScreenAddonPr
     public List<IFactory<? extends IScreenAddon>> getScreenAddons() {
         List<IFactory<? extends IScreenAddon>> screenAddons = Lists.newArrayList();
         if (defaultProvider != null) screenAddons.addAll(defaultProvider.getScreenAddons());
-        for (ItemApiLookup<?, Void> capability : capabilities) {
-            Object cap = capability.find(itemStack, null);
+        for (ItemApiLookup<?, ContainerItemContext> capability : capabilities) {
+            Object cap = capability.find(itemStack, ContainerItemContext.withInitial(itemStack));
             if (cap instanceof IScreenAddonProvider provider)
                 screenAddons.addAll(provider.getScreenAddons());
+        }
+        for (ComponentKey<?> capability : components) {
+            screenAddons.addAll(capability.maybeGet(itemStack)
+                .filter(cap -> cap instanceof IScreenAddonProvider)
+                .map(cap -> (IScreenAddonProvider)cap)
+                .map(IScreenAddonProvider::getScreenAddons)
+                .orElseGet(Lists::newArrayList));
         }
         return screenAddons;
     }
@@ -52,10 +63,17 @@ public class ItemStackHarness implements IContainerAddonProvider, IScreenAddonPr
     @Nonnull
     public List<IFactory<? extends IContainerAddon>> getContainerAddons() {
         List<IFactory<? extends IContainerAddon>> containerAddons = Lists.newArrayList();
-        for (ItemApiLookup<?, Void> capability : capabilities) {
-            Object cap = capability.find(itemStack, null);
+        for (ItemApiLookup<?, ContainerItemContext> capability : capabilities) {
+            Object cap = capability.find(itemStack, ContainerItemContext.withInitial(itemStack));
             if (cap instanceof IContainerAddonProvider provider)
                 containerAddons.addAll(provider.getContainerAddons());
+        }
+        for (ComponentKey<?> capability : components) {
+            containerAddons.addAll(capability.maybeGet(itemStack)
+                .filter(cap -> cap instanceof IContainerAddonProvider)
+                .map(cap -> (IContainerAddonProvider)cap)
+                .map(IContainerAddonProvider::getContainerAddons)
+                .orElseGet(Lists::newArrayList));
         }
         return containerAddons;
     }
